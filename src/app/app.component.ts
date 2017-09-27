ecec5be770f2acb2d031f545114d98aaa94bbbd6
Jothipriya Shekhar <jotitan27@gmail.com>
2017-09-27 11:12:47 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+public username : string = '';
+public password : string = '';
+public flag =  false;
+public users = [];
+public obj :any ={};
+
+changeusername(event){
+this.username = event.target.value;
+}
+
+changepassword(event){
+this.password = event.target.value;
+}
+onsave(){
+   this.flag = true;
+   this.obj.username= this.username;
+   this.obj.password = this.password;
+   this.users.push(this.obj);
+   this.obj = {};
+   console.log(this.users);
+
+}
+onclear(){
+  this.username = '';
+  this.password = '';
+}
 }
